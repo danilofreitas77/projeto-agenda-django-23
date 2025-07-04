527ac64ed4151ea908eb085906945f3c45920749
danilofreitas77 <danilocfreitass77@gmail.com>
2025-07-04 20:50:42 -0300

    personalizando o admin

--- contact/admin.py
+++ contact/admin.py
@@ -3,4 +3,6 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = '-id',
+    list_filter = 'created_date',
